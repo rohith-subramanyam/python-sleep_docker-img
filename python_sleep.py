fc03379253427c6952b0f1338372c5a072818fcc
Rohith Subramanyam <rohith.subramanyam@nutanix.com>
2018-10-12 17:28:19 -0700

    Log a line greater than 64 KiB to test if containersvcmon will be able to log it.

--- python_sleep.py
+++ python_sleep.py
@@ -2,10 +2,20 @@ import datetime
 import time
 
 def main():
+    for _ in xrange(20):
+        print "All I do is check time and sleep"
+        print "The time now is %s" % datetime.datetime.now()
+        print "Ok, I am going back to sleep now"
+
+        time.sleep(2)
+
+    # Testing log line greater that 64 KiB in containersvcmon.
+    print "*"*175254
+
     while True:
-        print("All I do is check time and sleep")
-        print("The time now is %s" % datetime.datetime.now())
-        print("Ok, I am going back to sleep now")
+        print "All I do is check time and sleep"
+        print "The time now is %s" % datetime.datetime.now()
+        print "Ok, I am going back to sleep now"
 
         time.sleep(2)
 
